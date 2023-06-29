7eddafb9c17294bae5f64c484555eef3a7a7444e
hanastasiia <nastia@r2h.in.ua>
2023-06-29 14:38:13 +0700

    .

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openMenulBtn: document.querySelector("[data-menu-open]"),
+      closeMenulBtn: document.querySelector("[data-menu-close]"),
+      menu: document.querySelector("[data-menu]"),
+    };
+  
+    refs.openMenulBtn.addEventListener("click", toggleMenu);
+    refs.closeMenulBtn.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle("is-hidden");
+      document.body.classList.toggle('no-scroll')
+    }
+  })();
